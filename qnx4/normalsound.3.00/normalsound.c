5971d306139efc7dd9b5e543f985f3d32bc3f73e
ksterne <kevintyler@vt.edu>
2021-11-08 15:14:40 +0000

    Commenting out xcf collection in sounding mode as gbr not able to collect xcf data

--- qnx4/normalsound.3.00/normalsound.c
+++ qnx4/normalsound.3.00/normalsound.c
@@ -457,7 +457,7 @@ int main(int argc,char *argv[]) {
       scan = -2;
 
       /* set the xcf variable to do cross-correlations (AOA) */
-      xcf = 1;
+      /* xcf = 1;  GBR does not currently do xcf data collection.  -KTS 20211108 */
 
       /* set the sounding mode integration time and number of ranges */
       intsc = snd_intt_sc;
