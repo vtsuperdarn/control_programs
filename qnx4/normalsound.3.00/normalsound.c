eda605722f055fd5e72c057ef3f8f3f13e11d539
egthomas <evan.g.thomas@dartmouth.edu>
2021-12-14 12:46:04 -0500

    Resolving conflicts with QNX4 normalsound 3.00 to 3.02 without directory naming

--- qnx4/normalsound.3.00/normalsound.c
+++ qnx4/normalsound.3.00/normalsound.c
@@ -46,6 +46,14 @@
 
 /*
  $Log: normalsound.c,v $
+ Revision 3.2  2021/12/03 egthomas
+ Modification to set default scan duration to 1-min
+ (fast) and allow 2-min operation via -slow option
+
+ Revision 3.1  2021/11/15 egthomas
+ Modification to set default nrang before SiteStart to
+ allow site-specific number of ranges for normal scan
+
  Revision 3.0  2021/09/15 egthomas
  Modification to use dmap sounding file format and
  independent number of ranges for frequency sounding
@@ -83,7 +91,7 @@
 #define TASK_NAMES "echo_data","iqwrite","rawacfwrite","fitacfwrite"
 
 char cmdlne[1024];
-char progid[80]={"$Id: normalsound.c,v 3.0 2021/09/15 egthomas Exp $"};
+char progid[80]={"$Id: normalsound.c,v 3.2 2021/12/03 egthomas Exp $"};
 char progname[256];
 struct TaskID *errlog;
 
